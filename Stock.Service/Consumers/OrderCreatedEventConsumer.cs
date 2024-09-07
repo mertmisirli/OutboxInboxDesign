926bdf247c7000614f4167fffc4f0748eb8b59dd
Furkan Mert Mısırlı <misirlifu@mef.edu.tr>
2024-09-07 23:35:08 +0300

    update order created event consumer

--- Stock.Service/Consumers/OrderCreatedEventConsumer.cs
+++ Stock.Service/Consumers/OrderCreatedEventConsumer.cs
@@ -1,13 +1,48 @@
 ﻿using MassTransit;
+using Microsoft.EntityFrameworkCore;
 using Shared.Events;
+using Stock.Service.Models.Contexts;
+using Stock.Service.Models.Entities;
+using System.Text.Json;
 
 namespace Stock.Service.Consumers
 {
     public class OrderCreatedEventConsumer : IConsumer<OrderCreatedEvent>
     {
-        public Task Consume(ConsumeContext<OrderCreatedEvent> context)
+        StockDbContext _stockDbContext;
+
+        public OrderCreatedEventConsumer(StockDbContext stockDbContext)
         {
-            throw new NotImplementedException();
+            _stockDbContext = stockDbContext;
+        }
+
+        public async Task Consume(ConsumeContext<OrderCreatedEvent> context)
+        {
+            var result = await _stockDbContext.OrderInboxes.AnyAsync(i => i.Id == context.Message.OrderId);
+            if (!result)
+            {
+
+                await _stockDbContext.OrderInboxes.AddAsync(new()
+                {
+                    Processed = false,
+                    Payload = JsonSerializer.Serialize(context.Message),
+                    Id = context.Message.OrderId
+                });
+
+                await _stockDbContext.SaveChangesAsync();
+            }
+
+
+            List<OrderInbox> orderInboxes = await _stockDbContext.OrderInboxes
+                .Where(i => i.Processed == false)
+                .ToListAsync();
+            foreach (var orderInbox in orderInboxes)
+            {
+                OrderCreatedEvent orderCreatedEvent = JsonSerializer.Deserialize<OrderCreatedEvent>(orderInbox.Payload);
+                Console.WriteLine($"{orderCreatedEvent.OrderId} order id değerine karşılık olan siparişin stok işlemleri başarıyla tamamlanmıştır.");
+                orderInbox.Processed = true;
+                await _stockDbContext.SaveChangesAsync();
+            }
         }
     }
 }
