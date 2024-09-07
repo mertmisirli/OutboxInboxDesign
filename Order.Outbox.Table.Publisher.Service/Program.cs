3c0e38fd5780ea3c93e4f2019549cf6409b06fc0
Furkan Mert Mısırlı <misirlifu@mef.edu.tr>
2024-09-07 15:40:50 +0300

    init project

---
+++ Order.Outbox.Table.Publisher.Service/Program.cs
@@ -0,0 +1,10 @@
+using Order.Outbox.Table.Publisher.Service;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
